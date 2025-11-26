ca96d70823edec4235a800cf7ca944c880f26c7f
akashpatelknit <patelakashmzp2001@gmail.com>
2025-11-27 01:13:01 +0530

    feat: setup commitlint

---
+++ commitlint.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat",
+        "fix",
+        "docs",
+        "style",
+        "refactor",
+        "perf",
+        "test",
+        "build",
+        "ci",
+        "chore",
+        "revert",
+      ],
+    ],
+    "subject-case": [2, "always", ["lower-case"]],
+  },
+};
+
+// 0 = disabled, 1 = warning, 2 = error
